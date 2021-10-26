e5f46c25d91649b2d65829882640eb13b77d1978
Mirko Jelic <mirkojelic.jelic@gmail.com>
2021-10-26 11:36:27 +0200

    change slider

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 // Navigation menu
 
 function openNav() {
-    document.getElementById('myNav').style.height = "100%";
-    document.querySelector('body').style.overflow = 'hidden';
+  document.getElementById('myNav').style.height = "100%";
+  document.querySelector('body').style.overflow = 'hidden';
 }
 function closeNav() {
-    document.getElementById('myNav').style.height = "0";
-    document.querySelector('body').style.overflow = 'visible';
+  document.getElementById('myNav').style.height = "0";
+  document.querySelector('body').style.overflow = 'visible';
 }
 
 // Pocetna - Image slider
@@ -14,54 +14,32 @@ function closeNav() {
 const mediaQuery = window.matchMedia('(max-width: 768px)');
 
 var slideIndex = 1;
-if(mediaQuery.matches) showSlides(slideIndex);
 
-function plusSlides(n) {
- showSlides(slideIndex += n);
+if (mediaQuery.matches) {
+  showSlides(slideIndex, 'slide_1')
+  showSlides(slideIndex, 'slide_2')
+};
+
+function plusSlides(n, className) {
+  showSlides(slideIndex += n, className);
 }
 
 function currentSlide(n) {
- showSlides(slideIndex = n);
+  showSlides(slideIndex = n, className);
 }
 
-function showSlides(n) {
+function showSlides(n, className) {
   var i;
-  var slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+  var slides = document.getElementsByClassName(className);
+  if (slides && mediaQuery.matches) {
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
   }
-  slides[slideIndex-1].style.display = "block";  
-}
-
-// Scroll button
-
-var scrollBtn = document.getElementById("scrollBtn");
-
-scrollBtn.addEventListener("click", scrollToTop, false);
-
-window.onscroll = function() {
-    scrollFunction()
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 950 || document.documentElement.scrollTop > 950) {
-    scrollBtn.style.display = "block";
-  } else {
-    scrollBtn.style.display = "none";
-  }
-}
 
 
-function scrollToTop() {
-    document.body.scrollTo({
-        top: 0, 
-        behavior: 'smooth'
-    });
-    document.documentElement.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
 }
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const sentMail = require('./js/mail')
+const path = require('path');
 
 const app = express()
 const router = express.Router()
@@ -13,12 +14,15 @@ app.use(express.static(__dirname))
 router.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
 router.get('/o-nama.html', (req, res) => res.sendFile(path.join(__dirname, 'o-nama.html')))
 router.get('/kontakt.html', (req, res) => res.sendFile(path.join(__dirname, 'kontakt.html')))
-router.get('*', (req, res) => res.send('Page Not found 404'));
+
 router.post('/kontakt.html', (req, res) => {
   const message = req.body
   console.log(message)
   sentMail(message)
   res.redirect('/kontakt.html')
+})
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
 })
 app.use('/', router);
 
